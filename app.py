29407872a55508fa25fee0e5c7967d8bbad0f9f8
clemens-steinwendner <clemens.steinwendner@gmail.com>
2025-03-11 20:01:16 +0100

    Working version including HSK chooser

--- app.py
+++ app.py
@@ -3,9 +3,50 @@ from story_generator import generate_sentence, generate_story
 from pypinyin import pinyin, Style
 import jieba
 import translators as ts
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///translations.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+punctuation_chars = set("，。！？；：,.!?;:")
+
+class WordCache(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    word = db.Column(db.String(100), unique = True, nullable = False)
+    pinyin = db.Column(db.String(200))
+    translation = db.Column(db.String(500))
+
+    def __repr__(self):
+        return f'WordCache {self.word}'
+    
+
+def get_pinyin_translation(word):
+    cached = WordCache.query.filter_by(word=word).first()
+    if cached:
+        return cached.pinyin, cached.translation
+    else:
+        # If not cached, compute
+        # pinyin
+        pinyin_list = pinyin(word, style=Style.TONE)
+        pinyin_str = ' '.join(item[0] for item in pinyin_list)
+        # translation
+        translation_str = ts.translate_text(
+            query_text=word,
+            translator='alibaba',
+            from_language='zh-CHS',
+            to_language='en'
+        ).lower()  # note the parentheses for .lower()
+
+        # Store in DB
+        new_entry = WordCache(word=word, pinyin=pinyin_str, translation=translation_str)
+        db.session.add(new_entry)
+        db.session.commit()
+
+        return pinyin_str, translation_str
 
 @app.route('/')
 def index():
@@ -17,16 +58,16 @@ def index():
     html_segments = []
 
     for seg in segments:
-        if seg.strip():
-            pinyin_list = pinyin(seg, style = Style.TONE)
-            pinyin_str = ' '.join([item[0] for item in pinyin_list])
+        if seg in punctuation_chars:
+            html_segments.append(seg)
+        else:
 
-            translation = ts.translate_text(query_text=seg, translator = 'alibaba', from_language='zh-CHS', to_language='en').lower()
+            pinyin, translation = get_pinyin_translation(seg)
 
 
             html_segments.append(
                 f'<span class="word" '
-                f'data-pinyin="{pinyin_str}" '
+                f'data-pinyin="{pinyin}" '
                 f'data-translation="{translation}">'
                 f'{seg}</span>'
             )
@@ -42,16 +83,16 @@ def index():
     html_segments_story = []
 
     for seg in segments_story:
-        if seg.strip():
-            pinyin_list = pinyin(seg, style = Style.TONE)
-            pinyin_str = ' '.join([item[0] for item in pinyin_list])
+        if seg in punctuation_chars:
+            html_segments_story.append(seg)
+        else:
 
-            translation = ts.translate_text(query_text=seg, translator = 'alibaba', from_language='zh-CHS', to_language='en').lower()
+            pinyin, translation = get_pinyin_translation(seg)
 
 
             html_segments_story.append(
                 f'<span class="word" '
-                f'data-pinyin="{pinyin_str}" '
+                f'data-pinyin="{pinyin}" '
                 f'data-translation="{translation}">'
                 f'{seg}</span>'
             )
@@ -64,25 +105,30 @@ def index():
 @app.route('/generate')
 def generate():
     #new_sentence = generate_sentence()
-    #new_story = generate_story()
-    new_story = "我喜欢喝水" #for testing
+
+    hsk_level = request.args.get('hsk_level', '1')
+    new_story = generate_story(hsk_level = hsk_level)
+    #new_story = "我喜欢喝水" #for testing
+
+    
+
+    print("generating..." + hsk_level)
 
     segments = list(jieba.cut(new_story))
 
     html_segments = [] 
 
     for seg in segments:
-        if seg.strip():
+        if seg in punctuation_chars:
+            html_segments.append(seg)
+        else:
 
-            pinyin_list = pinyin(seg, style = Style.TONE)
-            pinyin_str = ' '.join([item[0] for item in pinyin_list])
-
-            translation = ts.translate_text(query_text=seg, translator = 'alibaba', from_language='zh-CHS', to_language='en').lower()
+            pinyin, translation = get_pinyin_translation(seg)
 
 
             html_segments.append(
                 f'<span class="word" '
-                f'data-pinyin="{pinyin_str}" '
+                f'data-pinyin="{pinyin}" '
                 f'data-translation="{translation}">'
                 f'{seg}</span>'
             )
@@ -91,21 +137,26 @@ def generate():
 
     return jsonify({'sentence': final_html})
 
-@app.route('/lookup')
-def lookup():
-    word = request.args.get('word', '')
-
-    pinyin_list = pinyin(word, style = Style.TONE)
-    pinyin_str = ' '.join([item[0] for item in pinyin_list])
 
 
-
-    translation = ts.translate_text(query_text=word, translator = 'alibaba', from_language='zh-CHS', to_language='en').lower()
-
-
-    return jsonify({'pinyin': pinyin_str, 'translation': translation})
+#@app.route('/lookup')
+#def lookup():
+#    word = request.args.get('word', '')
+#
+#    pinyin_list = pinyin(word, style = Style.TONE)
+#    pinyin_str = ' '.join([item[0] for item in pinyin_list])
+#
+#
+#
+#    translation = ts.translate_text(query_text=word, translator = 'alibaba', from_language='zh-CHS', to_language='en').lower()
+#
+#
+#    return jsonify({'pinyin': pinyin_str, 'translation': translation})
 
 
 
 if __name__ == '__main__':
+
+    with app.app_context():
+        db.create_all()
     app.run(debug = True)
--- story_generator.py
+++ story_generator.py
@@ -18,12 +18,19 @@ def generate_sentence():
     result = stream.choices[0].message.content
     return result
 
-story_system_prompt = "Your task is to write a short story of around five sentences in simplified chinese. Use only HSK 1 vocabulary as much as possible. Make the story interesting."
+#story_system_prompt = "Your task is to write a short story of around three sentences in simplified chinese. Use only HSK 1 vocabulary as much as possible. Make the story interesting."
 
-def generate_story():
+def generate_story(hsk_level):
+
+    story_system_prompt = "Your task is to write a short story of around five sentences in simplified chinese. Use only HSK " + hsk_level+ " vocabulary as much as possible. Make the story interesting."
+
+    print("hello, really generating")
+
     stream = client.chat.completions.create(model = "gpt-4o-mini",
                                             messages = [{"role": "developer", "content": story_system_prompt}],
                                             stream = False)
+    
+    print(stream.choices[0].message.content)
     
     return stream.choices[0].message.content
     
